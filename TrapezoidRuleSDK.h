3c8c3d192642a807747842e625226de82ed4d9dc
Ezequiel Santos <ezequiel.ifsp@gmail.com>
2023-11-14 11:56:38 +0000

    Initial commit

---
+++ TrapezoidRuleSDK.h
@@ -0,0 +1,18 @@
+//
+//  TrapezoidRuleSDK.h
+//  TrapezoidRuleSDK
+//
+//  Created by Ezequiel dos Santos on 14/11/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TrapezoidRuleSDK.
+FOUNDATION_EXPORT double TrapezoidRuleSDKVersionNumber;
+
+//! Project version string for TrapezoidRuleSDK.
+FOUNDATION_EXPORT const unsigned char TrapezoidRuleSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TrapezoidRuleSDK/PublicHeader.h>
+
+
