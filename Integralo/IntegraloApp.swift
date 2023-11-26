f1f07eb434469e3fde7f546e995c03f6326c1618
Ezequiel Santos <ezequiel.ifsp@gmail.com>
2023-11-26 13:55:11 +0000

    add project files

---
+++ Integralo/IntegraloApp.swift
@@ -0,0 +1,17 @@
+//
+//  IntegraloApp.swift
+//  Integralo
+//
+//  Created by Ezequiel dos Santos on 14/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct IntegraloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
